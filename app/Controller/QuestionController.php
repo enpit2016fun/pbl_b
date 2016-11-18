b4a08919d11a1dd147fd07ffb4c5d6fc35802c95
宮井和輝 <b1012206@fun.ac.jp>
2016-11-18 22:20:49 +0900

    contextとcontent

--- app/Controller/QuestionController.php
+++ app/Controller/QuestionController.php
@@ -12,7 +12,7 @@ class QuestionController extends AppController {
    public function add() {
      $context = $this->request->data("Question.context");
      $point = $this->request->data("Question.point");
-     if ($this->request->isPost() && isset($context) && isset($point)) {
+     if ($this->request->isPost() && $context != "" && $point != "") {
        $this->Question->create();
        $this->Question->save($this->request->data);
        $this->redirect('http://175.184.29.233/cakephp/');
