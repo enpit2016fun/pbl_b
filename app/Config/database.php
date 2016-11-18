ad1fc812dfe9e02704e1734c54024855fa2acd71
宮井和輝 <b1012206@fun.ac.jp>
2016-11-18 16:35:41 +0900

    database.phpの修正

--- app/Config/database.php
+++ app/Config/database.php
@@ -72,7 +72,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => 'enpit2016',
-		'database' => 'meas_moods',
+		'database' => 'MeasMoodls',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
