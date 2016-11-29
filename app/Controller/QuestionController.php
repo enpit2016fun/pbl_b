9488bf4fa5f75d8bace30f820f740fc94cb7d880
宮井和輝 <b1012206@fun.ac.jp>
2016-11-29 21:38:50 +0900

    カテゴリが正常に取得できるか

--- app/Controller/QuestionController.php
+++ app/Controller/QuestionController.php
@@ -5,19 +5,26 @@ class QuestionController extends AppController {
   }
 
   public function measure() {
-     App::import('Model', 'Category');
-     $this->Category = new Category();
-     $category_list= $this->Category->find('all');
+    App::import('Model', 'Category');
+    $this->Category = new Category();
+    $category_list= $this->Category->find('all');
 
+    $result = array();
     foreach ($category_list as $category) {
       $cate_id = $category['Category']['id'];
       $cate = $category['Category']['category'];
-      print_r($cate_id);
-      print_r($cate);
+
+      $data = $this->Question->find('all', array(
+        'conditions' => array('Question.cate_id' => $cate_id),
+        'order' => 'rand()',    // ORDER BY RAND()
+        'limit' => 3            // 3件取得
+      ));
+
+      $result[] = $data;
     }
 
-     $data = $this->Question->find('all');
-     $this->set('data', $data);
+     //$data = $this->Question->find('all');
+     $this->set('data', $result);
   }
 
    public function add() {
